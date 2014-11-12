b0d1db7ea25918df6bdc30795886dc7967dc06c9
Mattia Albergante <monChiVoy8>
2014-11-12 16:31:05 +0100

    Added a build script

--- scripts/buildScript.sh
+++ scripts/buildScript.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 
 ROOT_DIR=../
-INCLUDE_DIR=../../includes/
-cd $ROOT_DIR/googletest/scripts
-
-
-mkdir -p $INCLUDE_DIR
-rm -rf $INCLUDE_DIR/gtest/
-
-python fuse_gtest_files.py $INCLUDE_DIR
-
+cmake .
+make
+./build/run_me
