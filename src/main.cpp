b97ce846fa36800d3860d1da0427e2179e16c7ce
Mattia Albergante <monChiVoy8>
2014-11-12 10:47:58 +0100

    Added example test in main, googletest as submodule (to be built independently)

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,28 @@
 #include <stdio.h>
 
+#include "gtest/gtest.h"
 
-int test() {
+using ::testing::EmptyTestEventListener;
+using ::testing::InitGoogleTest;
+using ::testing::Test;
+using ::testing::TestCase;
+using ::testing::TestEventListeners;
+using ::testing::TestInfo;
+using ::testing::TestPartResult;
+using ::testing::UnitTest;
+
+int TellMeOne() {
 	return 1;
 }
 
-int main() {
-  printf("hello world %d.\n", test());
-  return 0;
+TEST(TellMeOneTest, ReturnsRightValue) {
+  EXPECT_EQ(1, TellMeOne());
+}
+
+int main(int argc, char **argv) {
+	InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
+
+	// printf("I'm telling you one: %d.\n", TellMeOne());
+	// return 0;
 }
